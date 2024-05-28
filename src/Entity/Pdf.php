945b70e3e337260b4fd61ec913ea633e0fa47f85
julie <julie.quero@etudiant.univ-reims.fr>
2024-05-28 15:05:04 +0200

    Add entities tests + fix errors

--- src/Entity/Pdf.php
+++ src/Entity/Pdf.php
@@ -3,8 +3,6 @@
 namespace App\Entity;
 
 use App\Repository\PdfRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: PdfRepository::class)]
@@ -18,19 +16,12 @@ class Pdf
     #[ORM\Column(length: 255)]
     private ?string $title = null;
 
-    /**
-     * @var Collection<int, User>
-     */
-    #[ORM\ManyToMany(targetEntity: User::class, inversedBy: 'pdfs')]
-    private Collection $user_id;
-
     #[ORM\Column]
     private ?\DateTimeImmutable $created_at = null;
 
-    public function __construct()
-    {
-        $this->user_id = new ArrayCollection();
-    }
+    #[ORM\ManyToOne(inversedBy: 'pdfs')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $user_id = null;
 
     public function getId(): ?int
     {
@@ -49,30 +40,6 @@ class Pdf
         return $this;
     }
 
-    /**
-     * @return Collection<int, User>
-     */
-    public function getUserId(): Collection
-    {
-        return $this->user_id;
-    }
-
-    public function addUserId(User $userId): static
-    {
-        if (!$this->user_id->contains($userId)) {
-            $this->user_id->add($userId);
-        }
-
-        return $this;
-    }
-
-    public function removeUserId(User $userId): static
-    {
-        $this->user_id->removeElement($userId);
-
-        return $this;
-    }
-
     public function getCreatedAt(): ?\DateTimeImmutable
     {
         return $this->created_at;
@@ -81,6 +48,18 @@ class Pdf
     public function setCreatedAt(\DateTimeImmutable $created_at): static
     {
         $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getUserId(): ?User
+    {
+        return $this->user_id;
+    }
+
+    public function setUserId(?User $user_id): static
+    {
+        $this->user_id = $user_id;
 
         return $this;
     }
